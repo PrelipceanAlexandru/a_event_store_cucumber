c67c78520fa19586dec5f04a8c7511071d641e5c
Alex9508 <prelipceanalexg@gmail.com>
2019-05-05 14:28:26 +0300

    remove comments

--- features/step_definitions/tracking_item_step.rb
+++ features/step_definitions/tracking_item_step.rb
@@ -19,17 +19,3 @@ Then("there should be {int} events published for item stream {string}") do | eve
 
   expect(event_store.read.stream(stream_name).to_a.count).to eq event_numbers
 end
-# Then("The order of events for item {int} is: {string}") do |item_uid, events_order|
-#   debugger
-#   # expect(event_store).to have_published(an_event(Events::ItemCreated), an_event(Events::ItemUpdated), an_event(Events::ItemDeleted))
-#   expect(event_store).to have_published()
-# end
-
-# Then("The order of events for item {int} is: {string}") do |item_uid, events_order|
-#   debugger
-
-#   # events = [ an_event(Events::ItemCreated), an_event(Events::ItemUpdated), an_event(Events::ItemDeleted)]
-#   # expect(event_store.read.stream("Item$#{item_uid}").limit(3)).to eq(events)
-#   expect(event_store.read.stream("Item$#{item_uid}").to_a.map(&:class)).to eq([ an_event(Events::ItemCreated), an_event(Events::ItemUpdated), an_event(Events::ItemDeleted)])
-
-# end
