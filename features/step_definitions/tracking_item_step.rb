dc71ca5ff79d564335f343a0aa039b4207f24c65
alex <alex.prelipcean@yahoo.com>
2019-05-07 09:37:07 +0300

    Change from event_class to event_type

--- features/step_definitions/tracking_item_step.rb
+++ features/step_definitions/tracking_item_step.rb
@@ -5,19 +5,19 @@ end
 When("{string} event is published") do |event|
   stream_name = "Item$#{@data[:uid]}"
 
-  event = event_class(event).new(data: @data)
+  event = event_type(event).new(data: @data)
   event_store.publish(event, stream_name: stream_name)
 end
 
 Then("an {string} event should be published once for item {int}") do |event, uid|
   expect(event_store).to have_published(
-    an_event(event_class(event))).in_stream("Item$#{uid}").once
+    an_event(event_type(event))).in_stream("Item$#{uid}").once
 end
 
 Then("there should be {int} events published for item stream {string}") do | event_numbers, stream_name|
   expect(event_store.read.stream(stream_name).to_a.count).to eq event_numbers
 end
 
-def event_class(event)
+def event_type(event)
   "Events::#{event}".constantize
 end
